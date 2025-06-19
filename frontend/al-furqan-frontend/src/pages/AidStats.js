f74cffadc63bbcd1dc78f8bcb56a0bc151941a6a
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-19 22:20:07 +0300

    Update AidStats.js

--- frontend/al-furqan-frontend/src/pages/AidStats.js
+++ frontend/al-furqan-frontend/src/pages/AidStats.js
@@ -9,7 +9,7 @@ const AidStats = () => {
   const [summary, setSummary] = useState({ total_residents: 0, total_aids: 0 });
 
   useEffect(() => {
-    axios.get('https://al-furqan-project-uqs4.onrender.com/api/aids/stats')
+    axios.get('https://al-furqan-anas-new.onrender.com/api/aids/stats')
       .then(response => {
         const { daily_counts, aid_type_counts, total_residents, total_aids } = response.data;
         setAidCounts(daily_counts);

a1bc055ec8290f2693cc4942ab66052b9f3fadd4
Anas Abu Nahel <125291685+AnasAbuNahel@users.noreply.github.com>
2025-06-19 22:42:30 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ import json
 
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["https://al-furqan-project.vercel.app"])
+CORS(app, supports_credentials=True, origins=["https://al-furqan-anas-new.vercel.app"])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://db_al_alfurqan_user:Gtgu8PwBlB9fyoPjqiki66bYLpDieIRF@dpg-d101bjili9vc73dcets0-a/db_al_alfurqan'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
